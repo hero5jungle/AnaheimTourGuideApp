3b9fc7f2095692e5d1a6fa355d7872d878906f99
Tan Nguyen <ban5hien@yahoo.com>
2018-06-01 13:14:33 -0700

    Add a custom class

---
+++ app/src/main/java/com/example/android/anaheimtourguideapp/Location_Class.java
@@ -0,0 +1,31 @@
+package com.example.android.anaheimtourguideapp;
+
+public class Location_Class {
+    private static final int NO_IMAGE_PROVIDED = -1;
+    private int mLocationNameId;
+    private int mLocationAddressId;
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+    public Location_Class(int locationNameId, int locationAddressId, int imageResourceId) {
+        mLocationNameId = locationNameId;
+        mLocationAddressId = locationAddressId;
+        mImageResourceId = imageResourceId;
+    }
+
+    public int getLocationNameId() {
+        return mLocationNameId;
+    }
+
+    public int getLocationAddressId() {
+        return mLocationAddressId;
+    }
+
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+}
